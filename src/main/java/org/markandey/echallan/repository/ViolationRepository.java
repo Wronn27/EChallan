60364ebd0b39499d43556d52b2ffe7c06da70309
Markandey Upadhyay <22967markandeyupadhyay@gmail.com>
2026-04-09 04:23:57 +0530

    Changes to integrate with frontend

--- src/main/java/org/markandey/echallan/repository/ViolationRepository.java
+++
@@ -1,8 +0,0 @@
-package org.markandey.echallan.repository;
-
-import org.markandey.echallan.entity.Violation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ViolationRepository
-        extends JpaRepository<Violation, Long> {
-}
